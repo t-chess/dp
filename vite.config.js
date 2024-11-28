95a1182827a67fa5117706c4e2d733d42801d25c
Tatiana Chess <t.chess@outlook.com>
2024-11-28 10:16:18 +0100

    fix vite 2???

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
+import path from 'path'
+import fs from 'fs'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -6,11 +8,30 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   const outputDir = env.VITE_NODE_TYPE ? `build-${env.VITE_NODE_TYPE}` : 'build';
+  const publicDir = path.resolve(__dirname, 'public', env.VITE_NODE_TYPE);
 
   return {
+    publicDir,
     build: {
       outDir: outputDir,
     },
-    plugins: [react()],
+    plugins: [
+      react(),
+      {
+        name: 'html-transform',
+        transformIndexHtml(html) {
+          html = html.replace(
+            /<title>(.*?)<\/title>/,
+            `<title>${env.VITE_NODE_TYPE === 'config' ? 'Konfigurátor' : env.VITE_NODE_TYPE === 'data'?'Datová vizualizace':'Hra'}</title>`
+          );
+          if (env.VITE_NODE_TYPE === 'config') {
+            const jsonldContent = fs.readFileSync(path.resolve(__dirname, 'public', 'config/Configurator.jsonld'), 'utf-8');
+            const jsonldScript = `<script type="application/ld+json">${jsonldContent}</script>`;
+            html = html.replace('</head>', `${jsonldScript}\n</head>`);
+          }
+          return html;
+        },
+      },
+    ],
   }
 })
